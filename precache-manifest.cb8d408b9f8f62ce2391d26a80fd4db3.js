0a2f744716898775eb5b51a656e9b080b5a36103
forUAi <153248421+forUAi@users.noreply.github.com>
2024-07-30 15:55:23 -0500

    Updates

---
+++ precache-manifest.cb8d408b9f8f62ce2391d26a80fd4db3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "442833b1f275f589aad9cabbd1075002",
+    "url": "/vishal-portfolio/index.html"
+  },
+  {
+    "revision": "f0bda89d6cc7ef4644d5",
+    "url": "/vishal-portfolio/static/css/main.655d6c95.chunk.css"
+  },
+  {
+    "revision": "222527942db6e134137e",
+    "url": "/vishal-portfolio/static/js/2.93e3902b.chunk.js"
+  },
+  {
+    "revision": "12adf5ee6e135646e379",
+    "url": "/vishal-portfolio/static/js/3.8ba39429.chunk.js"
+  },
+  {
+    "revision": "f0bda89d6cc7ef4644d5",
+    "url": "/vishal-portfolio/static/js/main.3dce885e.chunk.js"
+  },
+  {
+    "revision": "b4bc1e3dd510505b8324",
+    "url": "/vishal-portfolio/static/js/runtime~main.6ec7ebe4.js"
+  },
+  {
+    "revision": "f59f69b70a57dead705721e6bae7481a",
+    "url": "/vishal-portfolio/static/media/image.f59f69b7.jpg"
+  }
+]);
